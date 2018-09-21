80dabb9995af1816f168f4353a7bbcc6a3c96504
David Asabina <vid@bina.me>
2018-09-21 09:17:19 +0200

    Remove refinedInvoice def + unused Nix pkgs :snowflake:

--- default.nix
+++ default.nix
@@ -1,35 +1,7 @@
 { stdenv, pkgs }:
 
-let
-  redefinedInvoice = let
-    invoice = pkgs.stdenv.mkDerivation rec {
-      version = "2011-10-01";
-      pname = "invoice";
-      tlType = "run";
-
-      name = "${pname}-${version}";
-
-      src = ./invoice;
-
-      buildInputs = with pkgs; [
-        tree
-        #unzip
-      ];
-
-      dontBuild = true;
-
-      unpackPhase = ''
-        echo "nothing to unpack";
-      '';
-
-      installPhase = ''
-        mkdir -p $out/tex/latex/invoice
-        cp -r $src/texinput $out/tex/latex/invoice
-      '';
-    };
-  in { pkgs = [ invoice ]; };
-in stdenv.mkDerivation rec {
-  name = "asabina-latex-${version}";
+stdenv.mkDerivation rec {
+  name = "asabina-latex-agreements-${version}";
   version = "0.1.0";
 
   buildInputs = with pkgs; [
@@ -38,8 +10,6 @@ in stdenv.mkDerivation rec {
       scheme-basic
       luatex
 
-      # biblatex
-      # blindtext
       collection-basic
       collection-fontsrecommended
       collection-langeuropean
@@ -49,31 +19,14 @@ in stdenv.mkDerivation rec {
       enumitem
       etoolbox
       fmtcount
-      # graphics-def
-      # IEEEtran
-      # lastpage
-      # layouts
-      # logreq
       numprint
       paracol
-      # pdfcrop
-      # realscripts
       tableof
-      # tabu
       tocloft
-      # varwidth
-      # xargs
       xetex
-      # xltxtra
-      # xstring
-      # xtab
       ;
     })
     zip
     unzip
   ];
-
-  #shellHook = ''
-  #  #export PATH="$PATH:${pkgs.arduino-core}/share/arduino/"
-  #'';
 }

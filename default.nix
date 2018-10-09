06a463db8aa6eb6a43ef359c37a9ceffb45c0535
David Asabina <vid@bina.me>
2018-10-09 21:25:41 +0200

    Add shellHook to fix backspace problem :snowflake:

--- default.nix
+++ default.nix
@@ -32,4 +32,10 @@ stdenv.mkDerivation rec {
     zip
     unzip
   ];
+
+  shellHook = ''
+    # https://github.com/NixOS/nix/issues/1056
+    export TERMINFO=/run/current-system/sw/share/terminfo
+    real_TERM=$TERM; TERM=xterm; TERM=$real_TERM; unset real_TERM
+  '';
 }
